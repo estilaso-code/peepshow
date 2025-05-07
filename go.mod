cc41d25b91bac04596e98ac9dcba59efd05a1401
El Pirata <elpirata@pop-os.localdomain>
2025-05-06 20:08:30 -0700

    Half finished Peepshow
    Have working greeting and input.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module peep
+
+go 1.24.2
+
+require github.com/estilaso-code/goinput v0.0.0-20250429230729-299c22105169 // indirect
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/estilaso-code/goinput"
+)
+
+func greetUser() {
+	fmt.Print(`
+ _____                _____ _                   
+|  __ \              / ____| |                  
+| |__) |__  ___ _ __| (___ | |__   _____      __
+|  ___/ _ \/ _ \ '_ \\___ \| '_ \ / _ \ \ /\ / /
+| |  |  __/  __/ |_) |___) | | | | (_) \ V  V / 
+|_|   \___|\___| .__/_____/|_| |_|\___/ \_/\_/  
+               | |                              
+               |_|                              
+`, "Welcome to the show! Lets peep some passwords.\n")
+}
+
+func passStrength() (string, error) {
+	complexity, err := goinput.Input("How hard do you want this password?\nType 1 for: So good and hard\nType 2 for: Kinda mid\nType 3 for: FLacid like your dick!\n")
+	if err != nil {
+		return "", fmt.Errorf("User can't follow instructions: %w", err)
+	}
+	return complexity, nil
+}
+
+func complexityChoice(complexity string) {
+	switch complexity {
+	case "1":
+		fmt.Println("Oh, you a big boy huh?")
+	case "2":
+		fmt.Println("Meh, kinda pansy, but whatever bro")
+	case "3":
+		fmt.Println("You little sissy boy! Grow some balls!!")
+	default:
+		fmt.Println("You are a real dumbass.")
+	}
+
+}
+
+func main() {
+	greetUser()
+	complexity, err := passStrength()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	complexityChoice(complexity)
+}
